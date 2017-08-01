87f99dc3fe6c6640629de38df073ce5fad15ec85
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2017-08-01 13:45:46 +0200

    Commented reconstruction lines from factorization script.

--- factorization_script.py
+++ factorization_script.py
@@ -35,8 +35,11 @@ if parameters is not None:
         print(param[0] + "\t" + str(param[1]))
 
 # Reconstruct the matrix; set tiny numbers < 1e-15 to 0
+# Uncomment if desired
+"""
 print("Reconstruct the matrix using output parameters: ")
 recon = sun_reconstruction(n, parameters)
 recon.real[np.abs(recon.real) < 1e-15] = 0.0
 recon.imag[np.abs(recon.imag) < 1e-15] = 0.0
 print(recon)
+"""
